aa9033146240d9c4bae877c3761dcc972ed2faaa
Volodya <chuchman002@gmail.com>
2023-11-30 23:26:20 +0200

    Add project files.

---
+++ Lab №8.1string/Lab №8.1string.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+bool containsAllLetters(const string& input) {
+    string target = "while";
+
+    for (char letter : target) {
+        if (input.find(letter) == string::npos) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+string replaceWhile(string input) {
+    size_t pos = 0;
+
+    while ((pos = input.find("while", pos)) != string::npos) {
+        input.replace(pos, 5, "**");
+        pos += 2; 
+    }
+
+    return input;
+}
+
+int main() {
+    string inputString;
+
+    cout << "Print line: ";
+    getline(cin, inputString);
+
+    if (containsAllLetters(inputString)) {
+        cout << "String contains all letters from 'while'.\n";
+    }
+    else {
+        cout << "String doesn`t contain all letters with 'while'.\n";
+    }
+
+    string result = replaceWhile(inputString);
+
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab №8.1string/Lab №8.1string.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestContainsAllLetters_Negative)
+		{
+			std::string testInput = "randomtext"; 
+
+			// Перевіряємо, чи функція containsAllLetters повертає false для рядка, який не містить усі літери з "while"
+			Assert::IsFalse(containsAllLetters(testInput));
+		}
+
+		TEST_METHOD(TestReplaceWhile)
+		{
+			std::string testInput = "while hello while world"; 
+
+			std::string expectedResult = "** hello ** world";
+			std::string actualResult = replaceWhile(testInput);
+
+			// Перевіряємо, чи функція replaceWhile виконує заміну правильно
+			Assert::AreEqual(expectedResult, actualResult);
+		}
+	};
+}
